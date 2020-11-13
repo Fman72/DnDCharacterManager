bb7251e4775d40cde56f808f40177be4f8ddc29a
Fraser Kearns <kearnsmain@gmail.com>
2020-11-13 10:24:32 +0000

    Getting userAuth urls working.

--- dndPartyManager/api/urls.py
+++ dndPartyManager/api/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('user', views.ModelView.as_view()),
     path('ability', views.AbilityView.as_view()),
+    path('character', views.CharacterView.as_view()),
+    path('character_class', views.CharacterClassView.as_view()),
+    path('ability_use', views.AbilityUseView.as_view()),
 ]
--- dndPartyManager/dndPartyManager/urls.py
+++ dndPartyManager/dndPartyManager/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/', include('api.urls')),
+    path('userAuth/', include('userAuth.urls')),
     path('admin/', admin.site.urls),
 ]
